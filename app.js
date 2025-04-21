24be3773bca31aed25f9e108cda230b2941c58e0
Mallank Gogri <mallankgogri1@gmail.com>
2025-04-21 13:30:38 +0530

    update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/doctor', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/doctormongodb+srv://mallankgogri1:mallankgogri1@cluster0.ojnt6ca.mongodb.net/?appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
