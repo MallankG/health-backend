9145cdadaa9a71718d2adf689c6770a6a544011f
Mallank Gogri <mallankgogri1@gmail.com>
2025-05-09 14:25:38 +0530

    Update 5

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,10 +3,15 @@ const User = require('../models/User');
 exports.searchUsers = async (req, res) => {
   try {
     const { q } = req.query;
-    if (!q) return res.json([]);
-    const users = await User.find({
-      name: { $regex: q, $options: 'i' }
-    }).select('_id name email role');
+    let users;
+    if (!q) {
+      // Return all users if no query provided (limit to 50 for safety)
+      users = await User.find().select('_id name email role').limit(50);
+    } else {
+      users = await User.find({
+        name: { $regex: q, $options: 'i' }
+      }).select('_id name email role');
+    }
     res.json(users);
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
